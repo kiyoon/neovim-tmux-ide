44756d39c689d6dc50b4e0ad621119ea70ffaf64
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-06 16:55:50 +0000

    fork template and tweak some

---
+++ lua/user/telescope.lua
@@ -0,0 +1,29 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+local trouble = require("trouble.providers.telescope")
+
+local actions = require "telescope.actions"
+
+telescope.setup {
+  defaults = {
+
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+
+    mappings = {
+      i = {
+        ["<Down>"] = actions.cycle_history_next,
+        ["<Up>"] = actions.cycle_history_prev,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<c-t>"] = trouble.open_with_trouble,
+      },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
+  },
+}
